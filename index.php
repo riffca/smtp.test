47e5c4604ef53f4373e05dfacc9fe82c1d45c060
riffca <riffca@yandex.ru>
2016-07-09 14:29:12 +0300

    it works

--- index.php
+++ index.php
@@ -1,31 +1,52 @@
 <?php
 
+$email = $_POST['email'];
+$text = $_POST['text'];
 
+if(isset($text) && isset($email)){
 
-require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 
+        //$transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+        //$transport = Swift_MailTransport::newInstance();
+        $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+            ->setUsername('riffcamailer@gmail.com')
+            ->setPassword('riffcamailer3286030');
 
-    //$transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
-    //$transport = Swift_MailTransport::newInstance();
-   /* $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
-        ->setUsername('riffcamailer@gmail.com')
-        ->setPassword('riffcamailer3286030'));
+        $mailer = Swift_Mailer::newInstance($transport);
 
-    $mailer = Swift_Mailer::newInstance($transport);
+        $message = Swift_Message::newInstance('Новая заявка')
+            ->setFrom(['riffcamailer@gmail.com' => 'Pаявка'])
+            ->setTo([$email, 'riffca@ya.ru'])
+            ->setBody($text);
 
-    $message = Swift_Message::newInstance('Новая заявка на Фото')
-        ->setFrom(['riffcamailer@gmail.com' => 'Фото заявка'])
-        ->setTo(['riffca@yandex.ru', 'anna-pliusnina@mail.ru'])
-        ->setBody("Заявка от Феди \n\n Контакт: 323");
+        $result = $mailer->send($message);
 
-    $result = $mailer->send($message);
+            if($result > 0) {
+                echo 'Сообщение выслано';
+            } else {
+                echo 'Не вышло';
+            }
+}
+?>
+<style>
+    label,input,textarea {
+        width: 50%;
+        display: block;
+    }
 
-        if($result > 0) {
-            echo 'yes';
-        } else {
-            echo 'no';
-        }*/
+    form {
+        max-width: 30%;
+        padding: 10px;
+    }
 
-        
-echo "1";
+</style>
+<form method="POST" action="index.php">
+<label>Введите email</label>
+<input type="email" name="email" require>
+<label>Введите текст</label>
+<textarea type="text" name="text" require></textarea>
+<input type="submit">
+</form>
+
 
